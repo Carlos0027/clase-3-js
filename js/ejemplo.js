88eafd6c0e19233d514e331137f837468dc16dcc
carlos0027 <carlosdavidyascuaran@gmail.com>
2025-05-09 10:09:16 -0500

    clase3js

---
+++ js/ejemplo.js
@@ -0,0 +1,65 @@
+if(5>7){
+    console.log("verdadero")
+}else{
+    console.log("false")
+}
+
+//let dato="cine"
+//let estadoTiempo="lloviendo"
+//if(dato=="cine" && estadoTiempo=="lloviendo"){
+  //  console.log("no salgas")}
+
+  let dato="cine"
+    let estadoTiempo="lloviendo"
+    if (estadoTiempo==="verano"){
+        console.log("voy a cine")
+    }else{
+        console.log("no salgas")
+    }
+
+    let x=10
+
+    if(x>5){
+        console.log("x es mayor que 5")
+    }
+    {
+        let z=20
+    }
+
+    if(valor === 1){
+        console.log("es uno")
+    }
+    if (valor === 2){
+        console.log("es dos")
+    }
+    if (valor === 3){
+        console.log("es tres")
+    }
+    if (valor === 4){
+        console.log("es cuatro")
+    }
+    if (valor === 5){
+        console.log("es cinco")
+    }
+    if (valor!== 1 && valor != 2 && valor != 3 && valor != 4 && valor != 5){
+        console.log("no es uno, dos, tres, cuatro o cinco")
+    }
+
+    if (valor === 1){
+        console.log("uno")
+    }
+    else if (valor === 2){
+        console.log("dos")
+    }
+    else if (valor === 3){
+        console.log("tres")
+    }
+    else if (valor === 4){
+        console.log("cuatro")
+    }
+    else if (valor === 5){
+        console.log("cinco")
+    }
+    else{
+        console.log("no es uno, dos, tres, cuatro o cinco")
+    }
